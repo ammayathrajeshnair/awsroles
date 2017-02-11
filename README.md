f9683c0c097c7a19f7fbe5506428065bfa53e51a
ammayathrajeshnair <rajesh.2.nair@gmail.com>
2017-02-11 22:12:10 +0000

    Project for AWS Roles

---
+++ README.md
@@ -0,0 +1,17 @@
+Ansible Roles for AWS Infrastructure Creation
+----------------------
+
+The purpose of these Ansible roles are to create the complete infrastructure over AWS using Ansible, which include:
+
+- EC2 Key Pair
+- Security Groups for EC2,RDS and ELB with tags
+- RDS instance
+
+I have also written so really small plugins to get the desired information.
+
+Requirement to use these roles:
+
+- Ansible v2.0
+- boto
+- AWS admin access
+
---
+++ lookup_plugins/get_sg_id_from_name.py
@@ -0,0 +1,23 @@
+'''
+Python code - Ammayath Rajesh Nair
+'''
+
+from ansible.errors import *
+from ansible.plugins.lookup import LookupBase
+
+try:
+    import boto
+    import boto.ec2
+except ImportError:
+    raise AnsibleError("get_sg_id_from_name lookup cannot be run without boto installed")
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+        region = terms[0][0]
+        sg_name = terms[0][1]
+        if isinstance(sg_name, basestring):
+            sg_name = sg_name
+        ec2_conn = boto.ec2.connect_to_region(region)
+        sg = ec2_conn.get_all_security_groups(filters={'group_name': sg_name})[0]
+        return [sg.id]
